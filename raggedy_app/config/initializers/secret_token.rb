b223c8f87b35d88a41cac7397ed005a917012586
Nikolina Lauc <ninalauc@gmail.com>
2013-10-17 10:41:41 +0100

    Set-up new Rails application raggedy_app

---
+++ raggedy_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RaggedyApp::Application.config.secret_key_base = '9f2f68a7d3a312e1ce8966787d4cdcc260f01e7232f6dad6b7312f550f260b999b19bd4319dfe628bcc8672f62256cefef76721aa43ee2387efb46de05f9c4ad'
---
+++ raggedy_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RaggedyApp::Application.config.session_store :cookie_store, key: '_raggedy_app_session'
