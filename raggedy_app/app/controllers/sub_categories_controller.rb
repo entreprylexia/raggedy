02c47953a16cc7a824bd22562cabcd511838c3a8
Nikolina Lauc <ninalauc@gmail.com>
2013-10-30 10:43:14 +0000

    Begin to add sizes

--- raggedy_app/app/controllers/sub_categories_controller.rb
+++ raggedy_app/app/controllers/sub_categories_controller.rb
@@ -7,5 +7,6 @@ class SubCategoriesController < ApplicationController
 	def show
 		@sub_category = SubCategory.find(params[:id])
 		@products = @sub_category.products
+		# @products = @products.where(size: params[:size]) if params[:size]
 	end
 end
