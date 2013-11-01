9f4c6dd82f0a087535f62876dcf75ede1995472f
Nikolina Lauc <ninalauc@gmail.com>
2013-11-01 10:57:19 +0000

    added user avatar using paperclip

--- raggedy_app/app/controllers/profiles_controller.rb
+++ raggedy_app/app/controllers/profiles_controller.rb
@@ -1,8 +1,27 @@
 class ProfilesController < ApplicationController
 
+	before_filter :check_user_matches_profile, only: [:edit, :update]
+
   def show
-     @user = User.find_by_username(params[:username])
+     @user = User.find_by_username(params[:id])
   end
 
+  def create
+  @user = User.create( params[:user] )
+  end
 
+  def edit
+	end
+
+	def update
+		@user.update(params[:user].permit(:avatar))
+		redirect_to profile_path(@user)
+	end 
+
+	private
+
+	def check_user_matches_profile
+  	@user = User.find_by_username(params[:id])
+		raise "That's not you!" if current_user != @user
+	end
 end
--- raggedy_app/app/models/user.rb
+++ raggedy_app/app/models/user.rb
@@ -2,10 +2,17 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable 
   has_many :products
 
   validates :username, uniqueness: true
   # has_many :products
+
+  def to_param
+  	username
+  end
 end
