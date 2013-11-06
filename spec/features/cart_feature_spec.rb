50f95f8b0079eafac98a3a2ed8d7e833abc4ceb6
Yuin <yhuang1223@gmail.com>
2013-11-06 18:10:55 +0000

    Add stripe and mailgun

--- spec/features/cart_feature_spec.rb
+++ spec/features/cart_feature_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe 'the cart' do 
 	let(:dress) {FactoryGirl.create(:product)}
+	let(:user) { FactoryGirl.create(:user)}
+	before(:each) { login_as(user, scope: :user) }
 
 	it 'should initally be empty' do 
 		visit shop_path(dress)
@@ -10,13 +12,13 @@ describe 'the cart' do
 
 	it 'can have items to it' do 
 		visit shop_path(dress)
-		click_button "Add to Cart" 
+		click_button "Add to Bag" 
 		expect(page).to have_css '.item_count', text: '1 item'
 	end 
 
 	it 'can remove an item' do 
 		visit shop_path(dress)
-		click_button "Add to Cart"
+		click_button "Add to Bag"
 		click_link '1 item'
 		click_link "Remove"
 		expect(page).to have_css '.item_count', text: '0 item'
